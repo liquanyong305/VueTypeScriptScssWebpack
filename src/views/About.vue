b5c530faf8ad146e07e047bd491c2aa6b4054705
liquanyong305 <liquanyong@gmail.com>
2019-04-08 19:10:50 +0900

    tempporary commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,40 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <div class="container">
+        <div v-if="profile.user">
+            <p>
+                Full name: {{ fullName }}
+            </p>
+            <p>
+                Email: {{ email }}
+            </p>
+        </div>
+        <div v-if="profile.error">
+            Oops an error occured
+        </div>
+    </div>
 </template>
+
+<script lang="ts">
+    import Vue from 'vue';
+    import { State, Action, Getter } from 'vuex-class';
+    import Component from 'vue-class-component';
+    import { ProfileState, User } from './store/profile/types';
+    const namespace: string = 'profile';
+@Component
+    export default class UserDetail extends Vue {
+        @State('profile') profile: ProfileState;
+        @Action('fetchData', { namespace }) fetchData: any;
+        @Getter('fullName', { namespace }) fullName: string;
+
+        mounted() {
+            // fetching data as soon as the component's been mounted
+            this.fetchData();
+        }
+
+        // computed variable based on user's email
+        get email() {
+            const user = this.profile && this.profile.user;
+            return (user && user.email) || '';
+        }
+    }
+</script>
